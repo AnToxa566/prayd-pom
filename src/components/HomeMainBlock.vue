33736c455fce253657b3ab7ecc880f56ec72bf21
Anton <71523607+AnToxa566@users.noreply.github.com>
2023-02-25 18:03:31 +0200

    feat: Main Block Completed

--- src/components/HomeMainBlock.vue
+++ src/components/HomeMainBlock.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="main-block">
-        <Header></Header>
+        <div class="container">
+            <Header></Header>
+
+            <home-main-block-offer class="main-block__offer"></home-main-block-offer>
+
+            <img class="main-block__img" src="images/offer.png" alt="Dog photo">
+        </div>
     </div>
 </template>
 
 
 <script>
 import Header from '@/components/Header.vue'
+import HomeMainBlockOffer from '@/components/HomeMainBlockOffer.vue'
 
 export default {
     components: {
         Header,
+        HomeMainBlockOffer,
     },
 }
 </script>
@@ -22,5 +30,33 @@ export default {
 
     height: 100vh;
     width: 100vw;
+
+    &__offer {
+        z-index: 2;
+
+        // margin-top: 230px;
+
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
+    &__img {
+        position: absolute;
+
+        max-width: 1060px;
+
+        bottom: 0;
+        right: 0;
+
+        z-index: 1;
+    }
+}
+
+.container {
+    max-width: 1410px;
+    height: 100%;
+
+    margin: 0 auto;
 }
 </style>
---
+++ src/components/HomeMainBlockOffer.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="offer">
+        <h1 class="offer-title">Здесь ты найдёшь своего друга</h1>
+
+        <p class="offer-subtitle">Профессиональный питомник померанских шпицев <span style="font-weight: 700;">Prayd Pom</span> исполнит вашу многолетнюю мечту!</p>
+    
+        <button class="offer-btn" to="/">Посмотреть щенков</button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style lang="scss" scoped>
+    .offer {
+        max-width: 570px;
+
+        &-title {
+            margin-bottom: 42px;
+        }
+
+        &-subtitle {
+            font-size: 30px;
+            font-weight: 500;
+            line-height: 150%;
+            letter-spacing: 0.015em;
+
+            margin-bottom: 42px;
+        }
+
+        &-btn {
+            color: white;
+            font-weight: 700;
+            font-size: 28px;
+
+            width: 450px;
+            height: 90px;
+
+            background: var(--pink-linear-gradient);
+            box-shadow: 0px 0px 15px 2px var(--light-pink);
+            border-radius: 60px;
+        }
+    }
+</style>
